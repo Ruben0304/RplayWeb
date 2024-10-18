9d5fa62d34d81282899e9cbf231b996c89aafa6b
Ruben Hernandez <hernandzruben9@gmail.com>
2024-10-17 21:27:48 -0400

    sass

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('confirmed-succed.html')
+    return render_template('index.html')
 
 
 @app.route('/exito-restablecer-contrasena')
